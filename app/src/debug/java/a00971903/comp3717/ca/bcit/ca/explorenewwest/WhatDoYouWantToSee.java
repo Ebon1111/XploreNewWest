c902c85a9088b120a6fb6e1516e6ffb7c5897b37
Francis <sonsofix@yahoo.ca>
2017-01-26 17:18:20 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

---
+++ app/src/debug/java/a00971903/comp3717/ca/bcit/ca/explorenewwest/WhatDoYouWantToSee.java
@@ -0,0 +1,13 @@
+package a00971903.comp3717.ca.bcit.ca.explorenewwest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WhatDoYouWantToSee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_what_do_you_want_to_see);
+    }
+}
